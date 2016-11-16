fa890db8a61e438be515fef6aeabb0cd3e2556fc
PCPC <ad654064778@qq.com>
2016-11-16 18:39:19 +0800

    个人中心

--- app/src/main/java/com/laichushu/book/ui/fragment/MineFragment.java
+++ app/src/main/java/com/laichushu/book/ui/fragment/MineFragment.java
@@ -3,8 +3,10 @@ package com.laichushu.book.ui.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
+import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.laichushu.book.R;
 import com.laichushu.book.ui.base.BasePresenter;
@@ -18,6 +20,7 @@ import com.laichushu.book.utils.UIUtil;
  * Created by wangtong on 2016/10/17.
  */
 public class MineFragment extends MvpFragment2 {
+    private TextView tvTitle;
     @Override
     protected BasePresenter createPresenter() {
 
@@ -26,12 +29,15 @@ public class MineFragment extends MvpFragment2 {
 
     @Override
     public View createSuccessView() {
-        View createSuccessView = UIUtil.inflate(R.layout.fragment_mine);
-        return createSuccessView;
+        View mRootView = UIUtil.inflate(R.layout.fragment_mine);
+        tvTitle =(TextView) mRootView.findViewById(R.id.tv_title);
+        tvTitle.setText("个人中心");
+        return mRootView;
     }
 
     @Override
     protected void initData() {
+        super.initData();
         refreshPage(LoadingPager.PageState.STATE_SUCCESS);
     }
 }
