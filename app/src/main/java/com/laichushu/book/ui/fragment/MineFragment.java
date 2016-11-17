ee4cc0ce094fa7d49acaa5f7717490d65f3c9aa7
PCPC <ad654064778@qq.com>
2016-11-17 10:27:08 +0800

    个人中心

--- app/src/main/java/com/laichushu/book/ui/fragment/MineFragment.java
+++ app/src/main/java/com/laichushu/book/ui/fragment/MineFragment.java
@@ -1,16 +1,11 @@
 package com.laichushu.book.ui.fragment;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.view.LayoutInflater;
-import android.view.TextureView;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.laichushu.book.R;
 import com.laichushu.book.ui.base.BasePresenter;
-import com.laichushu.book.ui.base.MvpFragment;
 import com.laichushu.book.ui.base.MvpFragment2;
 import com.laichushu.book.ui.widget.LoadingPager;
 import com.laichushu.book.utils.UIUtil;
@@ -19,8 +14,10 @@ import com.laichushu.book.utils.UIUtil;
  * 我的
  * Created by wangtong on 2016/10/17.
  */
-public class MineFragment extends MvpFragment2 {
+public class MineFragment extends MvpFragment2 implements View.OnClickListener {
     private TextView tvTitle;
+    private ImageView ivMineHead;
+
     @Override
     protected BasePresenter createPresenter() {
 
@@ -30,8 +27,9 @@ public class MineFragment extends MvpFragment2 {
     @Override
     public View createSuccessView() {
         View mRootView = UIUtil.inflate(R.layout.fragment_mine);
-        tvTitle =(TextView) mRootView.findViewById(R.id.tv_title);
-        tvTitle.setText("个人中心");
+        tvTitle = (TextView) mRootView.findViewById(R.id.tv_title);
+        ivMineHead = (ImageView) mRootView.findViewById(R.id.iv_minaHead);
+        tvTitle.setText("编辑个人信息");
         return mRootView;
     }
 
@@ -39,5 +37,10 @@ public class MineFragment extends MvpFragment2 {
     protected void initData() {
         super.initData();
         refreshPage(LoadingPager.PageState.STATE_SUCCESS);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
