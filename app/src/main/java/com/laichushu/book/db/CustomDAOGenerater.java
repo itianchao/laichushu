85158b7ca7c151596bba7e5f7b7c9337aa23759c
w11463269 <404944026@qq.com>
2016-11-24 18:19:54 +0800

    生成城市Id表

--- app/src/main/java/com/laichushu/book/db/CustomDAOGenerater.java
+++ app/src/main/java/com/laichushu/book/db/CustomDAOGenerater.java
@@ -15,19 +15,28 @@ public class CustomDAOGenerater {
          *  参数1：数据库版本
          *  参数2：数据库相关类文件所在的包名
          */
-        Schema schema = new Schema(1, "com.sofacity.laichushu.db");
+        Schema schema = new Schema(1, "com.laichushu.book.db");
         // 创建数据库表,参数为表名
         Entity entity = schema.addEntity("Search_History");
         // 为表添加字段
         entity.addIdProperty();// 该字段为id
         entity.addStringProperty("history");// String类型字段
-        // 生成数据库相关类
+        // 缓存表
         Entity cache = schema.addEntity("Cache_Json");
         // 为表添加字段
         cache.addIdProperty();// 该字段为id
         cache.addStringProperty("inter");// String类型字段
         cache.addStringProperty("json");// String类型字段
+        //城市表
+        Entity city = schema.addEntity("City_Id");
+        // 为表添加字段
+        city.addIdProperty();// 该字段为id
+        city.addStringProperty("province");
+        city.addStringProperty("city");
+        city.addIntProperty("proCode");
+        city.addIntProperty("cityCode");
+
         //第二个参数指定生成文件的本次存储路径,AndroidStudio工程指定到当前工程的java路径
-        new DaoGenerator().generateAll(schema, "D:\\code\\laichushu\\app\\src\\main\\java");
+        new DaoGenerator().generateAll(schema, "D:\\laichushu\\app\\src\\main\\java");
     }
 }
