d43be50f90b67406d27483b1a4c1225104c7ab1a
w11463269 <404944026@qq.com>
2016-12-06 10:09:57 +0800

    3

---
+++ app/src/main/java/com/laichushu/book/bean/netbean/PerInformation_Paramet.java
@@ -0,0 +1,42 @@
+package com.laichushu.book.bean.netbean;
+
+import java.io.Serializable;
+
+/**
+ * Created by PCPC on 2016/12/2.
+ */
+
+public class PerInformation_Paramet implements Serializable {
+
+    private String userId, pageNo, pageSize;
+
+    public PerInformation_Paramet(String userId, String pageNo, String pageSize) {
+        this.userId = userId;
+        this.pageNo = pageNo;
+        this.pageSize = pageSize;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPageNo() {
+        return pageNo;
+    }
+
+    public void setPageNo(String pageNo) {
+        this.pageNo = pageNo;
+    }
+
+    public String getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(String pageSize) {
+        this.pageSize = pageSize;
+    }
+}
