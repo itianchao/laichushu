822b0077385f0f5f3176f4664204fdca1a9788f3
PCPC <ad654064778@qq.com>
2016-12-19 13:56:28 +0800

    更新2016年12月19日13:56:18

--- app/src/main/java/com/laichushu/book/mvp/write/FindView.java
+++ app/src/main/java/com/laichushu/book/mvp/write/FindView.java
@@ -1,8 +1,14 @@
 package com.laichushu.book.mvp.write;
 
+import com.laichushu.book.mvp.home.HomeModel;
+
 /**
  * Created by PCPC on 2016/12/19.
  */
 
 public interface FindView {
+    void getDataSuccess(HomeModel model);
+    void getDataFail(String msg);
+    void showLoading();
+    void hideLoading();
 }
