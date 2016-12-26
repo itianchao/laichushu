865002d257231d2be05456517181052b893d2c6a
PCPC <ad654064778@qq.com>
2016-12-26 19:11:36 +0800

    修改消息

--- app/src/main/java/com/laichushu/book/global/ConstantValue.java
+++ app/src/main/java/com/laichushu/book/global/ConstantValue.java
@@ -88,12 +88,12 @@ public class ConstantValue {
 //    public static String API_SERVER_URL = "http://60.205.141.21:8099/";
 //    public static String API_SERVER_URL = "http://192.168.191.1:8082/book-app/";
 //    public static String API_SERVER_URL = "http://test2.laichushu.com/book-app-web/";
-      public static String API_SERVER_URL = "http://192.168.1.103:8082/book-app/";//张峰
+//      public static String API_SERVER_URL = "http://192.168.1.103:8082/book-app/";//张峰
 //    public static String API_SERVER_URL = "http://192.168.1.119:8082/book-app/";//施大勇1
 //    public static String API_SERVER_URL = "http://192.168.1.129:8082/book-app/";//施大勇2
 //    public static String API_SERVER_URL = "http://192.168.1.148:8082/book-app/";//施大勇3
 //    public static String API_SERVER_URL = "http://192.168.0.123:8082/book-app/";//施大勇4
-//    public static String API_SERVER_URL = "http://192.168.1.150:8082/book-app/";//施大勇5
+    public static String API_SERVER_URL = "http://192.168.1.150:8082/book-app/";//施大勇5
 //    public static String API_SERVER_URL = "http://192.168.1.130:8082/book-app/";//施大勇6
 //    public static String API_SERVER_URL = "http://192.168.147.101:8082/book-app/";//张永生
 //    public static String API_SERVER_URL = "http://test2.laichushu.com/book-app-web/";
